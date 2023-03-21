0e6fa3a7c4db424bd277276d9a23b7999c1dd148
marcin-zaczynski <mavix@op.pl>
2023-03-21 23:11:23 +0100

    dodano koty do listy

--- KotyMyszy/Koty i myszy.py
+++ KotyMyszy/Koty i myszy.py
@@ -2,14 +2,18 @@ import random
 import matplotlib
 import matplotlib.pyplot as plt
 
-class zwierze:
-    def __init__(self,imie):
+class zwierze():
+    def __init__(self,imie, kolor):
         self.imie = imie
         print(imie)
+        self.kolor = kolor
+
 class Kot(zwierze):
-    def __init__(self, imie, ruch):
-        super().__init__(imie)
+    def __init__(self, imie, ruch, kolor):
+        super().__init__(imie,kolor)
         self.ruch = ruch
+        self.x = []
+        self.y = []
 
 
 
@@ -27,10 +31,21 @@ class Kot(zwierze):
         if self.ruch[1] <= 0:
             self.ruch[1] += 5
 
+        self.x.append(self.ruch[0])
+        self.y.append(self.ruch[1])
+
+    def wykres(self):
+        plt.plot(self.x, self.y, ":.", color=self.kolor, linewidth=1, alpha=1, label='mysz')
+        plt.xlabel("lx")
+        plt.ylabel("ly")
+        plt.title("Ruchy kotów i myszy")
+        plt.grid(True)
+        plt.show()
+
 
 class Mysz(zwierze):
-    def __init__(self, imie, ruch):
-        super().__init__(imie)
+    def __init__(self, imie, ruch, kolor):
+        super().__init__(imie,kolor)
         self.ruch = ruch
         self.x = []
         self.y = []
@@ -51,27 +66,35 @@ class Mysz(zwierze):
 
     def wroc_na_poczatek(self):
         self.ruch = [5, 5]
+    def wykres(self):
+        plt.plot(self.x, self.y, ":.", color=self.kolor, linewidth=1, alpha=1, label='mysz')
+        plt.xlabel("lx")
+        plt.ylabel("ly")
+        plt.title("Ruchy kotów i myszy")
+        plt.grid(True)
+        plt.show()
 
-def dodanie_do_wspolrzednych(ktory):
-    if ktory == kot1:
-        lxk1.append(kot1.ruch[0])
-        lyk1.append(kot1.ruch[1])
-    elif ktory == kot2:
-        lxk2.append(kot2.ruch[0])
-        lyk2.append(kot2.ruch[1])
-    elif ktory == kot3:
-        lxk3.append(kot3.ruch[0])
-        lyk3.append(kot3.ruch[1])
 
-    else:
-        print('Błąd')
+#def dodanie_do_wspolrzednych(ktory):
+#    if ktory == kot1:
+#        lxk1.append(kot1.ruch[0])
+#        lyk1.append(kot1.ruch[1])
+#    elif ktory == kot2:
+#        lxk2.append(kot2.ruch[0])
+#        lyk2.append(kot2.ruch[1])
+#    elif ktory == kot3:
+#        lxk3.append(kot3.ruch[0])
+#        lyk3.append(kot3.ruch[1])
+
+#    else:
+#        print('Błąd')
 
 def wykres_krokow():
-    plt.plot(lxk1, lyk1, ":.", color="blue", linewidth=1, alpha=1, label='kot')
-    plt.plot(lxk2, lyk2, ":.", color="green", linewidth=1, alpha=1, label='kot')
-    plt.plot(mysz1.x, mysz1.y, ":.", color="yellow", linewidth=1, alpha=1, label='mysz')
-    plt.plot(mysz2.x, mysz2.y, ":.", color="purple", linewidth=1, alpha=1, label='mysz')
-    plt.plot(lxk3,lyk3, ':.',color='pink',linewidth=1,alpha=1, label='kot')
+    #plt.plot(kot1.x, kot1.y, ":.", color="blue", linewidth=1, alpha=1, label='kot')
+    #plt.plot(kot2.x, kot2.y, ":.", color="green", linewidth=1, alpha=1, label='kot')
+    #plt.plot(mysz1.x, mysz1.y, ":.", color="yellow", linewidth=1, alpha=1, label='mysz')
+    #plt.plot(mysz2.x, mysz2.y, ":.", color="purple", linewidth=1, alpha=1, label='mysz')
+    #plt.plot(kot3.x,kot3.y, ':.',color='pink',linewidth=1,alpha=1, label='kot')
     plt.xlabel("lx")
     plt.ylabel("ly")
     plt.title("Ruchy kotów i myszy")
@@ -79,29 +102,32 @@ def wykres_krokow():
     plt.show()
 
 
-kot1 = Kot('Przeciętniak1', [6, 6])
-kot2 = Kot('Przeciętniak2', [90, 100])
-kot3 = Kot('Tip Top', [1, 1])
-mysz1 = Mysz('Mysz1', [5, 5])
-mysz2 = Mysz('Mysz2', [50, 50])
-tablicaMyszy = [mysz1, mysz2]
+kot1 = Kot('Przeciętniak1', [6, 6],'blue')
+kot2 = Kot('Przeciętniak2', [90, 100],'green')
+kot3 = Kot('Tip Top', [1, 1],'yellow')
+tablicaKotow = [kot1, kot2, kot3]
+#mysz1 = Mysz('Mysz1', [5, 5], 'purple' )
+#mysz2 = Mysz('Mysz2', [50, 50], 'pink')
+tablicaMyszy = [Mysz('Mysz1', [5, 5], 'purple' ),Mysz('Mysz2', [50, 50], 'pink')]
 
-lxk1 = []
-lyk1 = []
-lxk2 = []
-lyk2 = []
-lxk3 = []
-lyk3 = []
+#lxk1 = []
+#lyk1 = []
+#lxk2 = []
+#lyk2 = []
+#lxk3 = []
+#lyk3 = []
 
 o = 0
 while o <= 1000:
     o += 1
-    kot1.rusz()
-    dodanie_do_wspolrzednych(kot1)
-    kot2.rusz()
-    dodanie_do_wspolrzednych(kot2)
-    kot3.rusz()
-    dodanie_do_wspolrzednych(kot3)
+    for i in tablicaKotow:
+        i.rusz()
+    #kot1.rusz()
+    #dodanie_do_wspolrzednych(kot1)
+    #kot2.rusz()
+    #dodanie_do_wspolrzednych(kot2)
+    #kot3.rusz()
+    #dodanie_do_wspolrzednych(kot3)
 
     for i in tablicaMyszy:
         i.rusz()
@@ -117,4 +143,8 @@ while o <= 1000:
             i.wroc_na_poczatek()
             print('spotkanie', kot3.imie, ' z ', i.imie)
 
-wykres_krokow()
+for i in tablicaMyszy:
+    i.wykres()
+for i in tablicaKotow:
+    i.wykres()
+#wykres_krokow()
