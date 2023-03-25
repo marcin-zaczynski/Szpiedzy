2ff4fac9c87c616c7bbb0b839ea3cbed564f312d
marcin-zaczynski <mavix@op.pl>
2023-03-25 22:23:33 +0100

    próba wyciągnięcia x i y  z punktów potrzebnych do wykresu

--- KotyMyszy/Koty i myszy.py
+++ KotyMyszy/Koty i myszy.py
@@ -9,12 +9,11 @@ class zwierze():
         self.kolor = kolor
 
 class Kot(zwierze):
-    def __init__(self, imie, ruch, kolor):
+    def __init__(self, imie, poczatek, kolor):
         super().__init__(imie,kolor)
-        self.ruch = ruch
-        self.historiax = []
-        self.historiay = []
-        self.punkt_kota = Punkt(ruch[0],ruch[1])
+        self.poczatek = poczatek
+        self.historia_kota = []
+        self.punkt_kota = Punkt(poczatek[0],poczatek[1])
     def rusz(self):
         self.punkt_kota.x += random.randint(-5, 5)
         self.punkt_kota.y += random.randint(-5, 5)
@@ -29,16 +28,16 @@ class Kot(zwierze):
         if self.punkt_kota.y <= 0:
             self.punkt_kota.y += 5
 
-        self.historiax.append(self.punkt_kota.x)
-        self.historiay.append(self.punkt_kota.y)
+        self.historia_kota.append(self.punkt_kota)
+
+
 
 class Mysz(zwierze):
-    def __init__(self, imie, ruch, kolor):
+    def __init__(self, imie, poczatek, kolor):
         super().__init__(imie,kolor)
-        self.ruch = ruch
-        self.historiax = []
-        self.historiay = []
-        self.punkt_myszy = Punkt(self.ruch[0],self.ruch[1])
+        self.poczatek = poczatek
+        self.historia_myszy = []
+        self.punkt_myszy = Punkt(self.poczatek[0],self.poczatek[1])
 
     def rusz(self):
         self.punkt_myszy.x += random.randint(-1, 1)
@@ -53,8 +52,7 @@ class Mysz(zwierze):
         if self.punkt_myszy.y >= 100:
             self.punkt_myszy.y -= 1
 
-        self.historiax.append(self.punkt_myszy.x)
-        self.historiay.append(self.punkt_myszy.y)
+        self.historia_myszy.append(self.punkt_myszy)
 
     def wroc_na_poczatek(self):
         self.ruch = [5, 5]
@@ -65,17 +63,28 @@ class Punkt:
         self.y = y
 
 def wykres_krokow():
-    for kot in tablicaKotow:
-        plt.plot(kot.historiax, kot.historiay, ":.", color=kot.kolor, linewidth=1, alpha=1, label='kot')
     for mysz in tablicaMyszy:
-        plt.plot(mysz.historiax, mysz.historiay, ":.", color=mysz.kolor, linewidth=1, alpha=1, label='mysz')
+        punkt_x = []
+        punkt_y = []
+        for polozenie in mysz.historia_myszy:
+            punkt_x.append(polozenie.x)
+            punkt_y.append((polozenie.y))
+        plt.plot(punkt_x,punkt_y, ":.", color=mysz.kolor, linewidth=1, alpha=1, label='mysz')
+    for kot in tablicaKotow:
+        punkt_x = []
+        punkt_y = []
+        for polozenie in kot.historia_kota:
+            punkt_x.append(polozenie.x)
+            punkt_y.append((polozenie.y))
+
+        plt.plot(punkt_x, punkt_y, ":.", color=kot.kolor, linewidth=1, alpha=1, label='kot')
     plt.xlabel("lx")
     plt.ylabel("ly")
     plt.title("Ruchy kotów i myszy")
     plt.grid(True)
     plt.show()
-
-tablicaKotow = [Kot('Przeciętniak1', [6, 6],'blue'), Kot('Przeciętniak2', [90, 100],'green'), Kot('Tip Top', [1, 1],'yellow')]
+Punkt.kot = (6,6)
+tablicaKotow = [Kot('Przeciętniak1',(6,6),'blue'), Kot('Przeciętniak2',(90, 1000),'green'), Kot('Tip Top',(1, 1),'yellow')]
 tablicaMyszy = [Mysz('Mysz1', [5, 5], 'purple' ),Mysz('Mysz2', [50, 50], 'pink'), Mysz('Jery',[90, 90], 'orange')]
 
 o = 0
@@ -88,7 +97,7 @@ while o <= 1000:
 
     for kot in tablicaKotow:
         for mysz in tablicaMyszy:
-            if kot.ruch == mysz.ruch:
+            if kot.historia_kota == mysz.historia_myszy:
                 print('spotkanie', kot.imie, 'z', mysz.imie)
                 mysz.wroc_na_poczatek()
 
