61fddb1c08578c33d617dc60b351a7641393b689
marcin-zaczynski <mavix@op.pl>
2023-03-26 10:27:29 +0200

    dodany przycisk dla kroku, zmieniony wykres tak aby działał

--- KotyMyszy/Koty i myszy.py
+++ KotyMyszy/Koty i myszy.py
@@ -1,6 +1,7 @@
 import random
 import matplotlib
 import matplotlib.pyplot as plt
+from tkinter import*
 
 class zwierze():
     def __init__(self,imie, kolor):
@@ -14,22 +15,25 @@ class Kot(zwierze):
         self.poczatek = poczatek
         self.historia_kota = []
         self.punkt_kota = Punkt(poczatek[0],poczatek[1])
+        self.punkt_x = []
+        self.punkt_y = []
     def rusz(self):
         self.punkt_kota.x += random.randint(-5, 5)
         self.punkt_kota.y += random.randint(-5, 5)
 
         if self.punkt_kota.x >= 100:
-            self.punkt_kota.x -= 5
+            self.punkt_kota.x = 99
         if self.punkt_kota.y >= 100:
-            self.punkt_kota.y -= 5
+            self.punkt_kota.y = 99
 
         if self.punkt_kota.x <= 0:
-            self.punkt_kota.x += 5
+            self.punkt_kota.x = 1
         if self.punkt_kota.y <= 0:
-            self.punkt_kota.y += 5
+            self.punkt_kota.y = 1
 
         self.historia_kota.append(self.punkt_kota)
-
+        self.punkt_x.append(self.punkt_kota.x)
+        self.punkt_y.append(self.punkt_kota.y)
 
 
 class Mysz(zwierze):
@@ -38,21 +42,25 @@ class Mysz(zwierze):
         self.poczatek = poczatek
         self.historia_myszy = []
         self.punkt_myszy = Punkt(self.poczatek[0],self.poczatek[1])
+        self.punkt_x = []
+        self.punkt_y = []
 
     def rusz(self):
         self.punkt_myszy.x += random.randint(-1, 1)
         self.punkt_myszy.y += random.randint(-1, 1)
 
         if self.punkt_myszy.x <= 0 :
-            self.punkt_myszy.x += 1
+            self.punkt_myszy.x = 1
         if self.punkt_myszy.y <=0:
-            self.punkt_myszy.y += 1
+            self.punkt_myszy.y = 1
         if self.punkt_myszy.x >= 100:
-            self.punkt_myszy.x -= 1
+            self.punkt_myszy.x = 99
         if self.punkt_myszy.y >= 100:
-            self.punkt_myszy.y -= 1
+            self.punkt_myszy.y = 99
 
         self.historia_myszy.append(self.punkt_myszy)
+        self.punkt_x.append(self.punkt_myszy.x)
+        self.punkt_y.append(self.punkt_myszy.y)
 
     def wroc_na_poczatek(self):
         self.ruch = [5, 5]
@@ -66,18 +74,19 @@ def wykres_krokow():
     for mysz in tablicaMyszy:
         punkt_x = []
         punkt_y = []
-        for polozenie in mysz.historia_myszy:
-            punkt_x.append(polozenie.x)
-            punkt_y.append((polozenie.y))
-        plt.plot(punkt_x,punkt_y, ":.", color=mysz.kolor, linewidth=1, alpha=1, label='mysz')
+        #for punkt in mysz.historia_myszy:
+        #    punkt_x.append(punkt.x)
+        #    punkt_y.append(punkt.y)
+        #plt.plot(punkt_x,punkt_y, ":.", color=mysz.kolor, linewidth=1, alpha=1, label='mysz')
+        plt.plot(mysz.punkt_x, mysz.punkt_y, ":.", color=mysz.kolor, linewidth=1, alpha=1, label='mysz')
     for kot in tablicaKotow:
-        punkt_x = []
-        punkt_y = []
-        for polozenie in kot.historia_kota:
-            punkt_x.append(polozenie.x)
-            punkt_y.append((polozenie.y))
-
-        plt.plot(punkt_x, punkt_y, ":.", color=kot.kolor, linewidth=1, alpha=1, label='kot')
+        #punkt_x = []
+        #punkt_y = []
+        #for polozenie in kot.historia_kota:
+        #    punkt_x.append(polozenie.x)
+        #    punkt_y.append(polozenie.y)
+        #plt.plot(punkt_x, punkt_y, ":.", color=kot.kolor, linewidth=1, alpha=1, label='kot')
+        plt.plot(kot.punkt_x, kot.punkt_y, ":.", color=kot.kolor, linewidth=1, alpha=1, label='kot')
     plt.xlabel("lx")
     plt.ylabel("ly")
     plt.title("Ruchy kotów i myszy")
@@ -88,8 +97,8 @@ tablicaKotow = [Kot('Przeciętniak1',(6,6),'blue'), Kot('Przeciętniak2',(90, 10
 tablicaMyszy = [Mysz('Mysz1', [5, 5], 'purple' ),Mysz('Mysz2', [50, 50], 'pink'), Mysz('Jery',[90, 90], 'orange')]
 
 o = 0
-while o <= 1000:
-    o += 1
+def krok():
+
     for kot in tablicaKotow:
         kot.rusz()
     for mysz in tablicaMyszy:
@@ -100,5 +109,17 @@ while o <= 1000:
             if kot.historia_kota == mysz.historia_myszy:
                 print('spotkanie', kot.imie, 'z', mysz.imie)
                 mysz.wroc_na_poczatek()
+                print(mysz.imie)
+    wykres_krokow()
+class kroki:
+    def __init__(self):
+        self.okno_przycisku = Tk()
+        self.okno_przycisku.geometry('140x100')
+        self.okno_przycisku.title('Kroki')
+        nazwa = Label(self.okno_przycisku, text='Zrób po jednym kroku')
+        nazwa.grid(column=0,row=0)
+        self.przycisk_krok = Button(self.okno_przycisku, text="Zrób krok ", command= krok)
+        self.przycisk_krok.grid(row=1, column=0)
+        self.okno_przycisku.mainloop()
 
-wykres_krokow()
+jeden_krok =kroki()
