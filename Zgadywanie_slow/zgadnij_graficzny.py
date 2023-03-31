bc1e5531b5b1448d0bcaa2c041a221dac06d9845
marcin-zaczynski <mavix@op.pl>
2023-03-31 18:57:30 +0200

    Dodano kolory do gry
    Gdy litera występuje w szukanym wyrazie tylko jest na złej pozycji podświetla się na czerwono, gdy jest na dobrym miejscu podświetla się na różowo i wskakuje do podpowiedzi u góry ekranu

--- Zgadywanie_slow/zgadnij_graficzny.py
+++ Zgadywanie_slow/zgadnij_graficzny.py
@@ -16,19 +16,24 @@ def display_info(do_sprawdzenia):
     sprawdz = do_sprawdzenia.get()
     ilosc_prob[0]+=1
     podane[0] = sprawdz
-    for litera in sprawdz:
-        if litera in slowo:
-            rozwiazanie[slowo.index(litera)] = litera
-            x = slowo.index(litera)
-            next = slowo[x + 1:]
-            if litera in str(next):
-                rozwiazanie[next.index(litera) + (x + 1)] = litera
     uzyte.append(sprawdz)
     wyswietl(sprawdz)
 
-def wyswietl(sprawdz):
+def wyswietl(sprawdz,):
+    for i in range(len(sprawdz)):
+        if sprawdz[i] == slowo[i]:
+            textbox.insert(tk.END, sprawdz[i], 'pink')
+            textbox.tag_config("pink", foreground="pink")
+            rozwiazanie[i] = sprawdz[i]
+
+        elif sprawdz[i] in slowo:
+            textbox.insert(tk.END, sprawdz[i], 'red')
+            textbox.tag_config("red", foreground="red")
+        else:
+            textbox.insert(tk.END, sprawdz[i])
+    textbox.insert(tk.END,'\n', ("p"))
     if podane[0] == slowo and ilosc_prob[0]<11:
-        rezultat.config(bg='yellow',text='Gratulacje,\n udało Ci się zgadnąć hasło za\n '+str(ilosc_prob[0])+' razem')
+        rezultat.config(bg='yellow',text='Gratulacje,\n udało Ci się zgadnąć hasło\n za '+str(ilosc_prob[0])+' razem')
         button.config(text='Zakończ',command=koniec)
     if ilosc_prob[0] > 10:
         rezultat.config(bg='red',text='Przegrałeś,\n odpowiedź to słowo\n '+slowo + ' \nspróbuj ponownie')
@@ -36,7 +41,7 @@ def wyswietl(sprawdz):
     entry.delete(0, 'end')
     info_prob.config(text='Pozostało ' + str(10 - int(ilosc_prob[0])) + ' prób')
     info_postep.config(text=rozwiazanie)
-    textbox.insert(tk.END, sprawdz+'\n', ("p"))
+
 
 def koniec():
     sys.exit()
@@ -56,14 +61,20 @@ info_label = tk.Label(window, text="Spróbuj odgadnąć słowo\n 5-cio literowe"
 info_label.pack()
 info_label.place(x=20, y=60)
 
+def handle_enter(event):
+    display_info(do_sprawdzenia)
 do_sprawdzenia = tk.StringVar()
 entry = tk.Entry(window, textvariable= do_sprawdzenia)
 entry.pack()
 entry.place(x=20, y=100)
+entry.bind("<Return>", handle_enter)
+
 def character_limit(entry_text):
     if len(do_sprawdzenia.get()) > 0:
         entry_text.set(entry_text.get()[:5])
 do_sprawdzenia.trace("w", lambda *args: character_limit(do_sprawdzenia))
+
+
 
 button = tk.Button(window, text="Zatwierdź", command=lambda : display_info(do_sprawdzenia))
 button.pack()
